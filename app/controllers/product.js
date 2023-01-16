06f495fb2b189a68e05df1f09486a7d819ba043f
shermondevelopment <victorshermon@yahoo.com>
2023-01-16 20:22:14 -0300

    feat: creating controller to receive request to get products

---
+++ app/controllers/product.js
@@ -0,0 +1,7 @@
+const SearchProduct = async (req, res) => {
+  // eslint-disable-next-line no-unused-vars
+  const { titleProduct } = req.query
+  res.sendStatus(200)
+}
+
+export default SearchProduct
